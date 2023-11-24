efae7d83c522a8348ff6b0a279c417d426390798
Gautam Raj <gautamraj996@gmail.com>
2023-11-24 11:15:24 +0530

    swiggy api data added

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
-const RestaurantCard = ({ image, name, rating, cuisines, place }) => {
+const RestaurantCard = ({
+  name,
+  avgRating,
+  cuisines,
+  place,
+  cloudinaryImageId,
+}) => {
   return (
     <>
-      <div className='restaurant-card'>
-        <img src={image} alt='' className='h-32 rounded w-44' />
+      <div className='flex-wrap border-2 restaurant-card w-[15%]'>
+        <img
+          src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
+          alt=''
+          className='h-32 rounded w-[100%]'
+        />
         <h3>{name}</h3>
-        <h4>{rating}</h4>
-        <h5>{cuisines.join(',')}</h5>
+        <h4>{avgRating}</h4>
+        <h5 className='word-wrap'>{cuisines.join(', ')}</h5>
         <h6>{place}</h6>
       </div>
     </>
